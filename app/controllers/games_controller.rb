a2ab733f806bf356c360037467f330fa44ce7bf1
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-08-10 16:59:02 +0200

    it plays

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,31 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
-    @letters = ["a".."z"].sample(random: SecureRandom)
+    @letters = []
+    10.times { |i| @letters << ('A'..'Z').to_a.sample }
   end
 
   def score
+    @word = params[:word].capitalize
+    @grid = params[:letters]
+    if  !english(@word) && !included?(@word, @grid)
+      @score = @word.length.to_i
+    else
+      @score = "Try again"
+    end
+  end
+
+  def english(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    word_dictionary = open(url).read
+    word = JSON.parse(word_dictionary)
+    @english = word[:found]
+  end
+
+
+  def included?(word, letters)
+    @grid = word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 end
